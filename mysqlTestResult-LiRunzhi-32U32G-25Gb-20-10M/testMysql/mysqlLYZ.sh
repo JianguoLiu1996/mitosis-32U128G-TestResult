c3447e4593e5573c1beff1623e9426634be934c2
JianguoLiu1996 <491925717@qq.com>
2023-07-26 09:21:08 +0800

    mysqlTestResult-LiRunzhi-On32U32G-25Gb-20-10M, 20230726.

---
+++ mysqlTestResult-LiRunzhi-32U32G-25Gb-20-10M/testMysql/mysqlLYZ.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+function prepareDB(){
+	sysbench --db-driver=mysql --mysql-host=localhost --mysql-port=3306 --mysql-user=root --mysql-password=123456 --mysql-db=testdb --table_size=10000000 --tables=20 --events=0 --time=300  --threads=200  oltp_read_write prepare
+}
+function testDB(){
+	sysbench /usr/share/sysbench/oltp_read_only.lua --mysql-host=127.0.0.1 --mysql-port=3306 --mysql-user=root --mysql-password=123456 --mysql-db=testdb4 --db-driver=mysql --tables=20 --table_size=10000000 --report-interval=15 --threads=100 --rand-type=gaussian --time=1800 run > GFM1800_FM_2nd.log 2>&1 &
+}
+#prepareDB
+#testDB
+sysbench /usr/share/sysbench/oltp_read_only.lua --mysql-host=127.0.0.1 --mysql-port=3306 --mysql-user=root --mysql-password=123456 --mysql-db=testdb4 --db-driver=mysql --tables=20 --table_size=10000000 --report-interval=3 --threads=100 --rand-type=gaussian --time=1800 run > GFM1800_testdb4_FM_1nd.log 2>&1 &
+wait
+sleep 1m
+
+sysbench /usr/share/sysbench/oltp_read_only.lua --mysql-host=127.0.0.1 --mysql-port=3306 --mysql-user=root --mysql-password=123456 --mysql-db=testdb4 --db-driver=mysql --tables=20 --table_size=10000000 --report-interval=3 --threads=100 --rand-type=gaussian --time=1800 run > GFM1800_testdb4_FM_2nd.log 2>&1 &
+wait
+sleep 1m
+
+sysbench /usr/share/sysbench/oltp_read_only.lua --mysql-host=127.0.0.1 --mysql-port=3306 --mysql-user=root --mysql-password=123456 --mysql-db=testdb4 --db-driver=mysql --tables=20 --table_size=10000000 --report-interval=3 --threads=100 --rand-type=gaussian --time=1800 run > GFM1800_testdb4_FM_3nd.log 2>&1 &
+wait
+sleep 1m
+
+sysbench /usr/share/sysbench/oltp_read_only.lua --mysql-host=127.0.0.1 --mysql-port=3306 --mysql-user=root --mysql-password=123456 --mysql-db=testdb4 --db-driver=mysql --tables=20 --table_size=10000000 --report-interval=3 --threads=100 --rand-type=gaussian --time=1800 run > GFM1800_testdb4_FM_4nd.log 2>&1 &
