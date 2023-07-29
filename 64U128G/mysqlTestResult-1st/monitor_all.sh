2ed775eee955811dc7c33227366e965e53046872
JianguoLiu1996 <491925717@qq.com>
2023-07-29 17:52:29 +0800

    mysql test result, on 64U128G, innodb buffer pool size is 100G, data size is 64,10M, enable page replication, 20230729.

---
+++ 64U128G/mysqlTestResult-1st/monitor_all.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+sudo ./monitor_memory.sh &
+MONITOR_MEMORY_PID=$!
+
+sudo ./monitor_io.sh &
+MONITOR_IO_PID=$!
+
+sudo ./monitor_network.sh &
+MONITOR_NETOWRK_PID=$!
+
+echo "monitor_memory_pid: $MONITOR_MEMORY_PID" >> monitor_all.log
+echo "monitor_io_pid: $MONITOR_IO_PID" >> monitor_all.log
+echo "monitor_network_pid: $MONITOR_NETOWRK_PID" >> monitor_all.log
+echo "sudo kill $MONITOR_MEMORY_PID $MONITOR_IO_PID $MONITOR_NETOWRK_PID" >> monitor_all.log
+
+# 等待3个小时
+sleep 3h
+sleep 10m
+sudo kill $MONITOR_MEMORY_PID $MONITOR_IO_PID $MONITOR_NETOWRK_PID
---
+++ 64U128G/mysqlTestResult-1st/mysqlTestBysysbench_innodb_buffer_pool_size100GB_64_10M_FM_OFF.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+NUMBER=1nd
+CONFIG=64_10M_FM_OFF
+INNODB_BUFFER_SIZE=100GB
+# OUTPUTPATH=./mysqlTestBysysbench_innodb_buffer_pool_size${INNODB_BUFFER_SIZE}_${CONFIG}_${NUMBER}/
+OUTPUTPATH=./
+MYSQLADDR="192.168.1.182"
+function preparedata(){
+	echo "SIGN: prepare data=="
+	sysbench --threads=400 \
+		--max-requests=0 \
+		--test=/home/jianguoliu/Download/sysbench-1.0.11/tests/include/oltp_legacy/oltp.lua \
+		--oltp-table-size=10000000 \
+		--oltp-tables-count=64 \
+		--mysql-db=testdb \
+		--db-driver=mysql \
+		--mysql-table-engine=innodb \
+		--mysql-host=${MYSQLADDR} \
+		--mysql-port=3306 \
+		--mysql-user=root \
+		--mysql-password=123456 \
+		--report-interval=3 prepare >> "mysql_test_result_innodbBufferSize${INNODB_BUFFER_SIZE}_${CONFIG}_${NUMBER}_$(date +"%Y%m%d%H%M%S").log"
+        wait
+        sleep 1m
+	echo "SIGN:data ready"
+}
+
+function cleardata(){
+	echo "==SIGN: can't cleardata=="
+}
+function testOne(){
+	echo "SIGN: stat one test."
+	sysbench /home/liujianguo/Download/sysbench-1.0.11/tests/include/oltp_legacy/oltp.lua \
+		--mysql-host=${MYSQLADDR} \
+		--oltp-tables-count=64 \
+		--mysql-user=root \
+		--mysql-password=123456 \
+		--mysql-port=3306 \
+		--db-driver=mysql \
+		--oltp-table-size=10000000 \
+		--mysql-db=testdb \
+		--max-requests=0 \
+		--oltp-simple-ranges=0 \
+		--oltp-distinct-ranges=0 \
+		--oltp-sum-ranges=0 \
+		--oltp-order-ranges=0 \
+		--max-time=3600 \
+		--oltp-read-only=on \
+		--threads=400 \
+		--report-interval=3 \
+		--thread-init-timeout=300 \
+		--forced-shutdown=1 run >> "${OUTPUTPATH}mysql_test_result_innodbBufferSize${INNODB_BUFFER_SIZE}_${CONFIG}_${NUMBER}_$(date +"%Y%m%d%H%M%S").log"
+	wait
+	sleep 1m
+	echo "SIGN: test one end."
+
+}
+function disableSWAP(){
+	sudo swapoff -a
+	wait
+	echo "SIGN: success disable swap."
+}
+function stopMYSQLandRedis(){
+	sudo service mysql stop
+	sudo service redis stop
+	sudo kill -9 $(ps aux | grep 'redis' | grep -v grep | tr -s ' '| cut -d ' ' -f 2)
+	sudo service mysql status
+	sudo service redis status
+}
+function disableAutoNUMA(){
+	# disable auto numa
+	echo 0 | sudo tee /proc/sys/kernel/numa_balancing > /dev/null
+	if [ $? -ne 0 ]; then
+		echo "ERROR setting AutoNUMA to: 0"
+                exit
+        fi
+	echo "SIGN:success disable Auto NUMA"
+}
+
+#three times test
+function alltest(){
+	start_time=$(date +%s)  # script start run time
+	
+	#rw test
+	for ((i=1; i<=3; i++))
+        do
+                NUMBER=${i}nd
+		testOne
+        done
+
+	end_time=$(date +%s)  # scrip stop run time,(s).
+	# calculate script run time,(s).
+	duration=$((end_time - start_time))
+	echo "Script run time is: $duration (s)"
+}
+#stopMYSQLandRedis
+disableSWAP
+disableAutoNUMA
+#preparedata
+#testOne
+#alltest
